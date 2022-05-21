6d6faa26a6ec26deed10edfadef443c9f4c1f67b
jacekhacking <Jacekhacking@mac.com>
2022-05-21 14:42:24 -0600

    resume PDF added Ahover elements corrected

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,10 +1,18 @@
 import React from "react";
-
+import Pdf from "../UI/Resume.pdf";
+import styles from "./Resume.module.css";
 const Resume = () => {
     return (
         <div>
-<p>
-    Download resume here!
+<p className={`${styles['resume-link']}`}>
+
+    <a
+        href={Pdf}
+        target="_blank"
+        rel="noreferrer"
+        style={{
+
+            }}> Download resume here!</a>
 
 </p>        </div>
     )
