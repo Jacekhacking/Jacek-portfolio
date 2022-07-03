fee6fa1f1078a3b0973864891aa6badbb49838ed
jacekhacking <Jacekhacking@mac.com>
2022-07-03 14:40:12 -0600

    altered about me container

--- src/components/Landing Page/LandingPage.js
+++ src/components/Landing Page/LandingPage.js
@@ -1,23 +1,30 @@
-import React, {Fragment} from "react";
+import React from "react";
 import styles from "./LandingPage.module.css";
 import selfie from "../UI/Images/Selfie_large.jpeg"
 
 const LandingPage = () => {
 
 
-    return (<Fragment>
-        <div id={'landing page'} className={`${styles['container']} bg-light`}>
-            {/*<h2 className={`${styles['about-text']} text-dark fs-800`}>HELLO! </h2>*/}
-            <div className={`${styles['dark-div']} text-dark fs-800 uppercase ff-cursive`}>
+    return (
+
+        <div style={{
+            alignItems:'center',
+            justifyContent:'center'
+        }}>
+            <div id={'landing page flex'} className={`${styles['container']} bg-light`}>
+                {/*<h2 className={`${styles['about-text']} text-dark fs-800`}>HELLO! </h2>*/}
+                <div className={`${styles['dark-div']} text-dark fs-800 uppercase ff-cursive`}>
 
                     <img src={selfie} alt="selfie"
                          className={`${styles['selfie-div']} `}/> <p
-                            className={`${styles['about-text']}`}><div style={{justifyContent:'center', alignItems:'center'}}>Hell0!</div> My name is
+                    className={`${styles['about-text']}`}><div style={{justifyContent:'center', alignItems:'center'}}>Hell0!</div> My name is
                     Jacek Hacking. I am a Front-End Web Developer. </p>
+                </div>
             </div>
         </div>
 
-    </Fragment>)
+
+    )
 }
 
 export default LandingPage
