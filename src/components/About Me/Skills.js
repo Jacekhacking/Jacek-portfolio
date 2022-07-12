3b19ed821d571e27102cc4da6a41e7c5920a52c5
jacekhacking <Jacekhacking@mac.com>
2022-07-11 19:44:02 -0600

    skills styles added

--- src/components/About Me/Skills.js
+++ src/components/About Me/Skills.js
@@ -3,25 +3,32 @@ import css from "../UI/Images/css_logo.png"
 import javascript from "../UI/Images/javascript_logo.png"
 import reactLogo from "../UI/Images/react_logo.png"
 import typeScript from "../UI/Images/typescript_logo.png"
-import styles from "./Skills.module.css";
+import styles from "./Skills.module.css"
 import UofUPng from "../UI/Images/coding-bootcamp-full-stack-developer-certificate.1.png"
-import React, {Fragment} from "react";
+import {StyledSkillsDiv, StyledSkillsImage} from "../UI/Styles/Skills.styles"
+
 
 const Skills = () => {
 
 
+    return (
+        <>
+            <h2 id="skills" className={`header-style ff-cursive text-dark`} style={{textAlign: 'center'}}>Skills</h2>
 
-    return(<Fragment>
-            <h2 id = "skills" className={`header-style ff-cursive text-dark`} style={{textAlign:'center'}}>Skills</h2>
-    <div className={styles['logo-container']}>
-            <img className={styles['logo']} src={html} alt="html logo"/>
-            <img className={styles['logo']} src={css} alt="css logo"/>
-            <img className={styles['logo']} src={javascript} alt="javascript logo"/>
-            <img className={styles['logo']} src={reactLogo} alt="react logo"/>
-            <img className={styles['logo']} src={typeScript} alt="typescript logo"/>
-            <img className={styles['logo']} src={UofUPng} alt="typescript logo"/>
-        </div>
-</Fragment>
+
+
+                <StyledSkillsDiv>
+                    <StyledSkillsImage src={html} alt="html logo"/>
+                    <StyledSkillsImage src={css} alt="html logo"/>
+                    <StyledSkillsImage src={javascript} alt="html logo"/>
+                    <StyledSkillsImage src={reactLogo} alt="html logo"/>
+                    <StyledSkillsImage src={typeScript} alt="html logo"/>
+
+
+                    <StyledSkillsImage src={UofUPng} alt="html logo"/>
+                </StyledSkillsDiv>
+
+        </>
     )
 }
 export default Skills;
---
+++ src/components/UI/Styles/Skills.styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+
+
+export const StyledSkillsDiv = styled.div`
+  padding-top: 1em;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin: 2em;
+
+
+  @media (max-width: 402px) {
+    max-width: 50px;
+    margin: .1em;
+  }
+`
+
+
+export const StyledSkillsImage = styled.img`
+  max-width: 100px;
+  height: auto;
+  margin: .3em;
+
+
+  @media (max-width: 402px) {
+    max-width: 50px;
+    margin: .1em;
+  }
+
+
+
+
+
+`
