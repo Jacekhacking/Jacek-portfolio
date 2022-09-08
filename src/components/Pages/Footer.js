729f2bc409caff78ce7afbbaa503ccb43b7bac27
JacekHacking <Jacekhacking@mac.com>
2022-09-07 22:59:44 -0600

    adding transitions and restyling social images onhover

--- src/components/Pages/Footer.js
+++ src/components/Pages/Footer.js
@@ -1,7 +1,6 @@
 import twitter from "../UI/Images/social logos/twitter.png";
 import github from "../UI/Images/social logos/GitHub-Mark-64px.png";
 import linkedin from "../UI/Images/social logos/linkedin.png";
-import { FlexContainer } from "../UI/Styles/Global.styles";
 
 import styled from "styled-components";
 
@@ -21,21 +20,37 @@ const Footer = () => {
         height: 100px;
       }
     }
-  `;
 
-  const FooterText = styled.p`
-    padding: 0.2rem;
-    font-size: clamp(var(--fs-200), 1.5vh, var(--fs-400));
-    display: flex;
-    color: hsl(var(--clr-mint-offwhite));
+    p {
+      padding: 0.2rem;
+      font-size: clamp(var(--fs-200), 1.5vh, var(--fs-400));
+      display: flex;
+      color: hsl(var(--clr-mint-offwhite));
 
-    a {
-      color: inherit;
-      font-size: inherit;
+      a {
+        color: inherit;
+        font-size: inherit;
+      }
+
+      @media (max-width: 300px) {
+        font-size: 1vh;
+      }
     }
 
-    @media (max-width: 300px) {
-      font-size: 1vh;
+    div {
+      display: flex;
+      div {
+        height: 60px;
+        width: 60px;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        &:hover {
+          background-color: black;
+          box-shadow: 0 0 10px hsl(var(--clr-sandy-brown));
+        }
+      }
     }
   `;
 
@@ -53,34 +68,41 @@ const Footer = () => {
 
   return (
     <FooterContainer>
-      <FooterText>
+      <p>
         Email:
         <a href="mailto:Jacekhacking@gmail.com"> JacekHacking@gmail.com</a>
-      </FooterText>
+      </p>
 
-      <FlexContainer>
-        <a
-          href="https://twitter.com/JH_WebDev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <SocialMediaImg src={twitter} alt="twitter" />
-        </a>
-        <a
-          href="https://github.com/Jacekhacking"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <SocialMediaImg src={github} alt="github" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/jacek-hacking-708007210"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <SocialMediaImg src={linkedin} alt="linked in " />
-        </a>
-      </FlexContainer>
+      <div>
+        <div>
+          <a
+            href="https://twitter.com/JH_WebDev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SocialMediaImg src={twitter} alt="twitter" />
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://github.com/Jacekhacking"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SocialMediaImg src={github} alt="github" />
+          </a>
+        </div>
+
+        <div>
+          <a
+            href="https://www.linkedin.com/in/jacek-hacking-708007210"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SocialMediaImg src={linkedin} alt="linked in " />
+          </a>
+        </div>
+      </div>
     </FooterContainer>
   );
 };
