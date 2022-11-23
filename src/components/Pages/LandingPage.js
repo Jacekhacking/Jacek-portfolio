bfec5b82a4b822c1dc2f07987e41a034d429f0bd
JacekHacking <Jacekhacking@mac.com>
2022-11-23 14:47:56 -0700

    style rework moved pokemon and axios call to different component

--- src/components/Pages/LandingPage.js
+++ src/components/Pages/LandingPage.js
@@ -1,21 +1,28 @@
-import SelfieImg from "../UI/Images/Selfie_large.jpeg";
+import SelfieImg from "../UI/Images/gc-rafting-picture.jpeg";
 import styled from "styled-components";
 const LandingPage = () => {
   const LandingPageWrapper = styled.div`
     display: flex;
+
     align-items: center;
-    justify-content: space-evenly;
-    flex-direction: row-reverse;
+    justify-content: center;
+    flex-direction: column;
     min-height: 95vh;
-    padding: 0 2rem;
+    /* padding: 0 2rem; */
     color: hsl(var(--clr-dark-gray));
+    div {
+      display: flex;
+    }
     p {
+      color: white;
     }
 
     img {
       height: 50rem;
       width: auto;
-      border: 4px solid hsl(var(--clr-dark-gray));
+      border-radius: 10px;
+      /* border: 4px solid hsl(var(--clr-dark-gray)); */
+      box-shadow: 0 0 20px var(--clr-4);
     }
     @media (max-width: 1000px) {
       padding: 0rem;
@@ -37,7 +44,7 @@ const LandingPage = () => {
   const AboutText = styled.div`
     /* max-width: 800px; */
     padding-right: 2rem;
-    p {
+    h2 {
       -webkit-text-stroke: 0.75px var(--clr-3);
       -webkit-text-fill-color: transparent;
       writing-mode: vertical-lr;
@@ -48,9 +55,6 @@ const LandingPage = () => {
     }
 
     @media (max-width: 1300px) {
-      p {
-        /* font-size: 5rem; */
-      }
     }
     @media (max-width: 1000px) {
       p {
@@ -63,12 +67,15 @@ const LandingPage = () => {
   return (
     <>
       <LandingPageWrapper>
-        <img src={SelfieImg} alt="Me Camping" />
+        <div>
+          <AboutText>
+            <h2>Hi, I'm Jacek!</h2>
+            {/* <p> My name is Jacek Hacking. I am a Front-End Web Developer</p> */}
+          </AboutText>
+          <img src={SelfieImg} alt="Me Camping" />
+        </div>
 
-        <AboutText>
-          <p>Hi, I'm Jacek!</p>
-          {/* <p> My name is Jacek Hacking. I am a Front-End Web Developer</p> */}
-        </AboutText>
+        <p>I'm a Front-End Web Developer from salt lake city Utah</p>
       </LandingPageWrapper>
     </>
   );
