edc01adf7608da59476501b6be18c2d29b5f6e0c
JacekHacking <Jacekhacking@mac.com>
2022-12-26 16:48:05 -0700

    work on projects file

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,14 +1,18 @@
 import { projectData } from "../data";
-import Skills from "../UI/Shared/Skills";
 
-import {
-  FlexWrapContainer,
-  FlexContainer,
-  OutLineStyledHeader,
-} from "../UI/Styles/Global.styles";
+import { OutLineStyledHeader } from "../UI/Styles/Global.styles";
 import styled from "styled-components";
 
 const Projects = () => {
+  const ProjectWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+  `;
+  const ProjectContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
   const ProjectDiv = styled.div`
     border: 2px solid hsl(var(--clr-dark-gray));
     margin: 0.5em;
@@ -18,38 +22,25 @@ const Projects = () => {
     background-image: url(${(props) => props.inputImg});
     background-size: contain;
     background-repeat: no-repeat;
-    background-position: center center;
-
-    a {
-      padding: 0;
-      margin: 0;
-    }
+    background-position: center;
 
     @media (max-width: 600px) {
       background-image: url(${(props) => props.mobileImage});
       width: 100%;
       height: 30rem;
     }
-
-    &:hover {
-      cursor: pointer;
-      border: 2px solid hsl(var(--clr-4-hsl));
-      div {
-        opacity: 1;
-        visibility: visible;
-        transition: opacity 0.2s linear, visibility 0.2s linear;
-      }
-    }
   `;
 
   const ProjectInfoDiv = styled.div`
     height: 500px;
     width: 500px;
-
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     text-align: center;
+
     background-color: hsl(var(--clr-dark-gray), 0.95);
     color: hsl(var(--clr-mint-offwhite));
-    transition: opacity 0.1s linear, visibility 0.1s linear;
 
     h2 {
       font-size: var(--fs-500);
@@ -62,11 +53,8 @@ const Projects = () => {
       }
     }
     p {
-      align-items: center;
       text-align: center;
       font-size: var(--fs-500);
-      padding: 1rem;
-      padding-bottom: 4rem;
     }
     @media (max-width: 600px) {
       p {
@@ -74,18 +62,12 @@ const Projects = () => {
       }
     }
 
-    div {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-
     a {
       font-family: var(--ff-cursive);
       background-color: transparent;
       font-weight: 900;
-      padding: 1rem;
-      margin: 0.5rem;
+      padding: 0 20px;
+      margin: 1rem;
       border: 4px solid hsl(var(--clr-mint-offwhite));
       box-shadow: none;
       border-radius: 0;
@@ -98,42 +80,34 @@ const Projects = () => {
       }
     }
   `;
-
-  const ProjectWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-  `;
   return (
     <>
-      <OutLineStyledHeader styles={{ paddingTop: "5rem" }}>
-        Proj
-      </OutLineStyledHeader>
-      {/* <Skills /> */}
-
+      <OutLineStyledHeader>Proj</OutLineStyledHeader>
       <ProjectWrapper>
         {projectData.map(
           ({ Image, Name, Description, GitHub, Link, MobileImage }) => (
-            <FlexContainer key={Name}>
+            <ProjectContainer key={Name}>
+              <ProjectInfoDiv>
+                <div>
+                  <h2>{Name}</h2>
+                  <p>{Description} </p>
+                </div>
+
+                <div>
+                  <a href={GitHub} target="_blank" rel="noopener noreferrer">
+                    GitHub
+                  </a>
+                  <a href={Link} target="_blank" rel="noopener noreferrer">
+                    Live Site
+                  </a>
+                </div>
+              </ProjectInfoDiv>
+
               <ProjectDiv
                 inputImg={Image}
                 mobileImage={MobileImage}
               ></ProjectDiv>
-
-              <div>
-                <ProjectInfoDiv>
-                  <h2>{Name}</h2>
-                  <p>{Description} </p>
-                  <FlexContainer>
-                    <a href={GitHub} target="_blank" rel="noopener noreferrer">
-                      GitHub
-                    </a>
-                    <a href={Link} target="_blank" rel="noopener noreferrer">
-                      Live Site
-                    </a>
-                  </FlexContainer>
-                </ProjectInfoDiv>
-              </div>
-            </FlexContainer>
+            </ProjectContainer>
           )
         )}
       </ProjectWrapper>
