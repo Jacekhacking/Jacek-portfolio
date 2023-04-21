f8b2e5ce3d428a53ec403334722ca8597e7a65a1
JacekHacking <Jacekhacking@mac.com>
2023-04-21 13:39:56 -0600

    added middle ground project removed keaton project changed minor styling to be responsive

--- src/components/data.js
+++ src/components/data.js
@@ -1,47 +1,47 @@
 export const projectData = [
   {
-    Name: "LCBodywork",
+    Name: 'Middle Ground ',
     Description:
-      "Professional Website for LC Bodyworks. A local massage therapist looking for a professional website.",
-    Image: require("./UI/Images/LCBodyworkSC.png"),
-    MobileImage: require("./UI/Images/Small-LCBodyworkSC.png"),
-    Link: "https://www.lcbodywork.com",
-    GitHub: "https://github.com/jacekhacking/lc-bodywork",
+      'Website for local massage therapist business Middle Ground Treatment & Training. ReactJS CSS and hosted with AWS Amplify',
+    Image: require('./UI/Images/middle-ground-full-ss.png'),
+    MobileImage: require('./UI/Images/middle-ground-ss.png'),
+    Link: 'https://middle-ground-production.up.railway.app/',
+    GitHub: 'https://github.com/Jacekhacking/Middle-Ground',
   },
   {
-    Name: "YouTube Clone",
+    Name: 'LCBodywork',
     Description:
-      " YouTube Clone built alongside JavascriptMastery youtube video. Built with ReactJS, ReactRouter, MUI, RapidAPI",
-    Image: require("./UI/Images/YoutubeSS.png"),
-    MobileImage: require("./UI/Images/YoutubeSS.png"),
-    Link: "https://glittering-scone-de1925.netlify.app",
-    GitHub: "https://github.com/jacekhacking/jsm_youtube_clone",
+      'Professional Website for LC Bodyworks. A local massage therapist looking for a professional website.',
+    Image: require('./UI/Images/LCBodyworkSC.png'),
+    MobileImage: require('./UI/Images/Small-LCBodyworkSC.png'),
+    Link: 'https://www.lcbodywork.com',
+    GitHub: 'https://github.com/jacekhacking/lc-bodywork',
   },
   {
-    Name: "Bee Better Massage",
+    Name: 'YouTube Clone',
     Description:
-      "Website for local massage therapist business Bee Better Massage. ReactJS CSS and hosted with AWS Amplify",
-    Image: require("./UI/Images/bee-better-ss.png"),
-    MobileImage: require("./UI/Images/Small-bee-better-ss.png"),
-    Link: "https://www.Beebettermassage.com",
-    GitHub: "https://github.com/Jacekhacking/BeeBetter",
+      ' YouTube Clone built alongside JavascriptMastery youtube video. Built with ReactJS, ReactRouter, MUI, RapidAPI',
+    Image: require('./UI/Images/YoutubeSS.png'),
+    MobileImage: require('./UI/Images/YoutubeSS.png'),
+    Link: 'https://glittering-scone-de1925.netlify.app',
+    GitHub: 'https://github.com/jacekhacking/jsm_youtube_clone',
   },
 
+  // {
+  //   Name: 'Art Portfolio',
+  //   Description:
+  //     'Website for local artist. Client designed the site himself and needed someone with technical skills to apply it.',
+  //   Image: require('./UI/Images/keaton-port-ss.png'),
+  //   MobileImage: require('./UI/Images/Small-keaton-port-ss.png'),
+  //   GitHub: 'https://github.com/jacekhacking/Keaton_Portfolio',
+  // },
   {
-    Name: "Art Portfolio",
+    Name: 'OneWaveGolf',
     Description:
-      "Website for local artist. Client designed the site himself and needed someone with technical skills to apply it.",
-    Image: require("./UI/Images/keaton-port-ss.png"),
-    MobileImage: require("./UI/Images/Small-keaton-port-ss.png"),
-    GitHub: "https://github.com/jacekhacking/Keaton_Portfolio",
-  },
-  {
-    Name: "OneWaveGolf",
-    Description:
-      "Website for OneWaveGolf. A company based out of Bandon Oregon specializing in recyclable golf goods, professional caddying, and lessons.",
-    Image: require("./UI/Images/onewavegolf.png"),
-    MobileImage: require("./UI/Images/Small-onewavegolf.png"),
-    Link: "https://www.onewavegolf.com",
-    GitHub: "https://github.com/jacekhacking/OneWaveGolf",
+      'Website for OneWaveGolf. A company based out of Bandon Oregon specializing in recyclable golf goods, professional caddying, and lessons.',
+    Image: require('./UI/Images/onewavegolf.png'),
+    MobileImage: require('./UI/Images/Small-onewavegolf.png'),
+    Link: 'https://www.onewavegolf.com',
+    GitHub: 'https://github.com/jacekhacking/OneWaveGolf',
   },
 ];
